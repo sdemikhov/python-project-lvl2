08e79f31767f37b2e185fa0e7d350f1980fd8ab6
python <python@debian>
2020-04-14 10:18:11 -0500

    starts step6: new feature - recursive diff, make prototype

--- gendiff/gendiff_lib.py
+++ gendiff/gendiff_lib.py
@@ -12,110 +12,136 @@ def run():
 
 
 def generate_diff(path_to_file1, path_to_file2):
-    file1 = load_file(path_to_file1)
-    file2 = load_file(path_to_file2)
+    data_file1 = load_file(path_to_file1)
+    data_file2 = load_file(path_to_file2)
 
-    difference = make_difference(file1, file2)
+    difference = make_difference(data_file1, data_file2)
 
-    return format_difference_plain(difference)
+    return stringify_difference(
+        difference,
+        indent_level = 0,
+        indent_type = '  '
+    )
 
 
-def make_difference(file1, file2):
-    difference = {
-        'same': {},
-        'edited': {},
-        'deleted': {},
-        'added': {},
-    }
+def make_difference(data_file1, data_file2):
+    difference = {}
 
-    keys_file1 = set(file1.keys())
-    keys_file2 = set(file2.keys())
+    keys_file1 = set(data_file1.keys())
+    keys_file2 = set(data_file2.keys())
 
     shared_keys = keys_file1 & keys_file2
     unique_keys_file1 = keys_file1 - keys_file2
     unique_keys_file2 = keys_file2 - keys_file1
-
-    for shared_key in shared_keys:
-        if file1[shared_key] == file2[shared_key]:
-            difference['same'][shared_key] = file1[shared_key]
-        else:
-            difference['edited'][shared_key] = {
-                'before': file1[shared_key],
-                'after': file2[shared_key],
-            }
-
+    
     for unique_key1 in unique_keys_file1:
-        difference['deleted'][unique_key1] = file1[unique_key1]
+        difference[unique_key1] = {
+            'type': 'deleted_element',
+            'value': data_file1[unique_key1],
+        }
 
     for unique_key2 in unique_keys_file2:
-        difference['added'][unique_key2] = file2[unique_key2]
+        difference[unique_key2] = {
+            'type': 'added_element',
+            'value': data_file2[unique_key2],
+        }
 
+    for shared_key in shared_keys:
+        if data_file1[shared_key] == data_file2[shared_key]:
+            difference[shared_key] = {
+            'type': 'unchanged_element',
+            'value': data_file1[shared_key],
+        }
+        else:
+            shared_values = (data_file1[shared_key], data_file2[shared_key])
+            if all(isinstance(value, dict) for value in shared_values):
+                difference[shared_key] = {
+                    'type': 'container',
+                    'value': make_difference(*shared_values),
+                }
+            else:
+                difference[shared_key] = {
+                    'type': 'edited_element',
+                    'value': {
+                        'before': data_file1[shared_key],
+                        'after': data_file2[shared_key],
+                    }
+                }
     return difference
 
 
-def get_same_fields(difference):
-    return difference['same']
+def stringify_difference(difference, indent_level, indent_type):
+    indent = indent_type * indent_level
+    result = '{indent}{{\n'.format(indent = indent)
+    for key, value in difference.items():
+        if value.get('type') == 'container':
+            container = stringify_difference(
+                value.get('value'),
+                indent_level + 1,
+                indent_type
+            )
+            element_as_string = '{indent}{key}: {container}'.format(
+                indent=indent_type * (indent_level + 1),
+                key=key,
+                container=container,
+            )
+        elif value.get('type').endswith('unchanged_element'):
+            element_as_string = stringify_unchanged_element(
+                {key: value.get('value')},
+                indent_type * (indent_level + 1)
+            )
+        elif value.get('type').endswith('edited_element'):
+            element_as_string = stringify_edited_element(
+                {key: value.get('value')},
+                indent_type * (indent_level + 1)
+            )
+        elif value.get('type').endswith('deleted_element'):
+            element_as_string = stringify_deleted_element(
+                {key: value.get('value')},
+                indent_type * (indent_level + 1)
+            )
+        elif value.get('type').endswith('added_element'):
+            element_as_string = stringify_added_element(
+                {key: value.get('value')},
+                indent_type * (indent_level + 1)
+            )
+        result += element_as_string
+    result += '{indent}}}\n'.format(indent = indent)
+    return result
 
 
-def get_edited_fields(difference):
-    return difference['edited']
+def stringify_unchanged_element(element, indent):
+    template = '{indent}{key}: {value}\n'
+    return element_to_string(element, template, indent)
 
 
-def get_deleted_fields(difference):
-    return difference['deleted']
-
-
-def get_added_fields(difference):
-    return difference['added']
-
-
-def format_difference_plain(difference):
-    template = '{{\n{same}{edited}{deleted}{added}}}\n'
-
-    same = stringify_same_fields(get_same_fields(difference))
-    edited = stringify_edited_fields(get_edited_fields(difference))
-    deleted = stringify_deleted_fields(get_deleted_fields(difference))
-    added = stringify_added_fields(get_added_fields(difference))
-
-    return template.format(
-        same=same,
-        edited=edited,
-        deleted=deleted,
-        added=added,
-    )
-
-
-def stringify_same_fields(fields):
-    template = '    {key}: {value}\n'
-    return fields_to_string(fields, template)
-
-
-def stringify_edited_fields(fields):
-    fields_as_string = ''
-    template = ('  + {key}: {after}\n  - {key}: {before}\n')
-    for key, value in fields.items():
+def stringify_edited_element(element, indent):
+    element_as_string = ''
+    template = ('{indent}+ {key}: {after}\n{indent}- {key}: {before}\n')
+    for key, value in element.items():
         before = value['before']
         after = value['after']
-        fields_as_string += template.format(
+        element_as_string += template.format(
+            indent=indent,
             key=key,
             before=before,
             after=after,
         )
-    return fields_as_string
+    return element_as_string
 
 
-def stringify_deleted_fields(fields):
-    template = '  - {key}: {value}\n'
-    return fields_to_string(fields, template)
+def stringify_deleted_element(element, indent):
+    template = '{indent}- {key}: {value}\n'
+    return element_to_string(element, template, indent)
 
 
-def stringify_added_fields(fields):
-    template = '  + {key}: {value}\n'
-    return fields_to_string(fields, template)
+def stringify_added_element(element, indent):
+    template = '{indent}+ {key}: {value}\n'
+    return element_to_string(element, template, indent)
 
 
-def fields_to_string(fields, template):
-    fields_as_string = ''
-    for key, value in fields.items():
-        fields_as_string += template.format(key=key, value=value)
-    return fields_as_string
+def element_to_string(element, template, indent):
+    element_as_string = ''
+    for key, value in element.items():
+        element_as_string += template.format(indent=indent,key=key, value=value)
+    return element_as_string
